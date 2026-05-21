a3538833018b26f773649fe7ba7eee8505fbc206
MRHOM <MRHOM@LAPTOP-9RSIE69G>
2026-05-21 16:18:27 -0600

    Create code for assignment 9

---
+++ Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarFile
+{
+    public class Car
+    {
+        public string model;
+        public int speed;
+        public Car(string Model, int Speed)
+        {
+            model = Model;
+            Speed = 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// This program will focus on creating and returning objects from methods
+// Austen Hernandez
+// 5/21/26
+
+using CarFile;
+
+namespace ProgramFile
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var UserModel = CreateCar().model;
+            Console.WriteLine("Car Created: " + UserModel);
+        }
+        static Car CreateCar()
+        {
+            Console.WriteLine("Enter Car model: ");
+            string input = Console.ReadLine();
+            Car car1 = new Car(input, 0);
+            return car1;
+        }
+    }
+}
